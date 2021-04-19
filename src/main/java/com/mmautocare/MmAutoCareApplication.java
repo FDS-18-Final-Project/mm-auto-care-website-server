694a69c82494f1928fb83b7ee75a6d35e964619a
JunSeong <ebseud6135@naver.com>
2021-04-19 23:57:30 +0900

    feat: project init

---
+++ src/main/java/com/mmautocare/MmAutoCareApplication.java
@@ -0,0 +1,13 @@
+package com.mmautocare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmAutoCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmAutoCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mmautocare/MmAutoCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmautocare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmAutoCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
