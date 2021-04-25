52631730cb2a40e7406246c8f7b6ee2a1f60f6e0
JunSeong <ebseud6135@naver.com>
2021-04-26 00:25:39 +0900

    test: delete QuotationRepositoryTest

---
+++ src/main/java/com/mmautocare/quotation/api/exceptionhandler/QuotationRestControllerAdvice.java
@@ -0,0 +1,10 @@
+package com.mmautocare.quotation.api.exceptionhandler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class QuotationRestControllerAdvice {
+
+}
