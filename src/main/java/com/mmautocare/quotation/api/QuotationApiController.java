1f7bcac061b4745efb9aff5173f8923273a3aafc
JunSeong <ebseud6135@naver.com>
2021-04-26 01:04:12 +0900

    refactor: post 요청 시 requestBody 사용

--- src/main/java/com/mmautocare/quotation/api/QuotationApiController.java
+++ src/main/java/com/mmautocare/quotation/api/QuotationApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +35,7 @@ public class QuotationApiController {
     }
 
     @PostMapping
-    public ResponseEntity<Long> save(Quotation quotation) {
+    public ResponseEntity<Long> save(@RequestBody Quotation quotation) {
         Quotation save = quotationService.save(quotation);
         return ResponseEntity.created(URI.create("/" + save.getId())).build();
     }
